a155435597f7059aea6222a8d481f7f13bf426dc
Dalton Smith <daltzsmith@gmail.com>
2023-03-12 16:15:12 -0400

    Now loads to proper sectors, imported date_util.asm

--- Makefile
+++ Makefile
@@ -12,17 +12,21 @@ BUILD_DIR   = build
 
 MBR         = bootloaderV.asm
 LDR         = kernelV.asm
+DATE		= date_utilV.asm
 MBR_SRC     = $(subst V,$(VER),$(MBR))
 MBR_BIN     = $(subst .asm,.bin,$(MBR_SRC))
 LDR_SRC     = $(subst V,$(VER),$(LDR))
 LDR_BIN     = $(subst .asm,.bin,$(LDR_SRC))
+DATE_SRC	= $(subst V,$(VER),$(DATE))
+DATE_BIN	= $(subst .asm,.bin,$(DATE_SRC))
 
 .PHONY : everything clean blankimg
 
 
-everything : pre-build $(BUILD_DIR)/$(MBR_BIN) $(BUILD_DIR)/$(LDR_BIN) $(IMG)
+everything : pre-build $(BUILD_DIR)/$(MBR_BIN) $(BUILD_DIR)/$(LDR_BIN) $(BUILD_DIR)/$(DATE_BIN) $(IMG)
 	dd if=$(BUILD_DIR)/$(MBR_BIN) of=$(BUILD_DIR)/$(IMG) bs=512 count=1 conv=notrunc
-	dd if=$(BUILD_DIR)/$(LDR_BIN) of=$(BUILD_DIR)/$(IMG) bs=512 count=2 seek=1 conv=notrunc
+	dd if=$(BUILD_DIR)/$(LDR_BIN) of=$(BUILD_DIR)/$(IMG) bs=512 count=2 seek=37 conv=notrunc
+	dd if=$(BUILD_DIR)/$(DATE_BIN) of=$(BUILD_DIR)/$(IMG) bs=512 count=1 seek=38 conv=notrunc
 
 pre-build:
 	mkdir -p $(BUILD_DIR)
@@ -31,6 +35,9 @@ $(BUILD_DIR)/$(MBR_BIN) : $(ASM_DIR)/$(MBR_SRC)
 	$(ASM) $(ASMFLAGS) $< -o $@
 
 $(BUILD_DIR)/$(LDR_BIN) : $(ASM_DIR)/$(LDR_SRC)
+	$(ASM) $(ASMFLAGS) $< -o $@
+
+$(BUILD_DIR)/$(DATE_BIN) : $(ASM_DIR)/$(DATE_SRC)
 	$(ASM) $(ASMFLAGS) $< -o $@
 
 $(IMG) :
--- asm/bootloader.asm
+++ asm/bootloader.asm
@@ -18,54 +18,57 @@ start:
 					;Colors from 8: Gray LBlue LGreen LCyan LRed LMagenta Yellow BWhite
 
 
-;;;load 2nd sector and run
+;;;load splash program and run
 	mov bx, 0x0001			;es:bx input buffer, temporary set 0x0000:1234
 	mov es, bx
 	mov bx, 0x2345
 	mov ah, 02h				;Function 02h (read sector)
 	mov al, 2				;Read two sectors
 	mov ch, 0				;Cylinder#
-	mov cl, 2				;Sector# --> 2 has program
+	mov cl, 38				;Sector# --> 37 (38-1) has program
 	mov dh, 0				;Head# --> logical sector 1
 	mov dl, 0				;Drive# A, 08h=C
 	int 13h
 
 	push cs
-	lea ax, [continue]
+	lea ax, [after_splash]
 	push ax
 	jmp 0x0001:0x2345	;Run program on sector 1, ex:bx
 	
-continue:
+after_splash:
+	;;load datetime utility
+	mov bx, 0x0002			;es:bx input buffer, temporary set 0x0000:1234
+	mov es, bx
+	mov bx, 0x3456
+	mov ah, 02h				;Function 02h (read sector)
+	mov al, 2				;Read two sectors
+	mov ch, 0				;Cylinder#
+	mov cl, 39				;Sector# --> 38 (39-1) has program
+	mov dh, 0				;Head# --> logical sector 1
+	mov dl, 0				;Drive# A, 08h=C
+	int 13h
+
+	push cs
+	lea ax, [after_dateutil]
+	push ax
+	jmp 0x0002:0x3456	;Run program
+
+after_dateutil:
 	call cls
-	call print_str
 	int 20h
 
-cls:
-	mov ah,06h		;Function 06h (scroll screen)
-	mov al,0		;Scroll all lines
-	mov bh,1eh		;Attribute (set background color, 7 stands for white for cursor color)
-	mov ch,0		;Upper left row is zero
-	mov cl,0		;Upper left column is zero
-	mov dh,24		;Lower left row is 24
-	mov dl,79		;Lower left column is 79
-	int 10h			;BIOS Interrupt 10h (video services)
-					;Colors from 0: Black Blue Green Cyan Red Magenta Brown White
-					;Colors from 8: Gray LBlue LGreen LCyan LRed LMagenta Yellow BWhite
-					;Only 8 colors for background, 16 for cursor
-	ret
-
-print_str:
-	mov bl,0x0E		;Set string color
-	mov bh,00h		;Set background color, always set both bl and bh when pushing text
-	mov ah,13h		;Function 13h (display string), XT machine only
-	mov al,1		;Write mode is zero: cursor stay after last char
-	mov cx,1		;Character string length
-	mov dh,22		;Position on row 0
-	mov dl,27		;And column 0
-	mov bp, 5	;Load the offset address of string into BP, es:bp
-					;Same as mov bp, msg
-	int 10h
-	ret
+;; CLS is a testing function
+;; Basically used as "print("i'm here")"
+cls:			 
+  mov ah,06h	;function 06h (Scroll Screen)
+  mov al,0	;scroll all lines
+  mov bh,1FH	;Attribute (bright white on blue)
+  mov ch,0	;Upper left row is zero
+  mov cl,0	;Upper left column is zero
+  mov dh,24	;Lower left row is 24
+  mov dl,79	;Lower left column is 79
+  int 10H	;BIOS Interrupt 10h (video services)
+  ret
 
 padding	times 510-($-$$) db 0		;to make MBR 512 bytes
 bootSig	db 0x55, 0xaa		;signature (optional)
